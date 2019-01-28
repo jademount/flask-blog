646307d1335bb657d68705bc809853caf4f87104
jademount <linhw1978@gmail.com>
2019-01-29 07:18:09 +0800

    20190129 from mix2

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
-
 from app import app
 from flask import render_template,flash,redirect,url_for
+
 from app.forms import LoginForm
 @app.route("/")
 @app.route("/index")
+
 def index():
     user={'username':'Daniel'}
     posts = [{
@@ -17,10 +18,9 @@ def index():
 
 @app.route('/login',methods=['GET','POST'])
 
-
-
 def login():
     form=LoginForm()
+    #form.validate_on_submit() returns True if methods=POST, False if methods=GET 
     if form.validate_on_submit():
         flash('User {} Login, remember me={}'.format(form.username.data,form.remember_me.data))
         return redirect(url_for('index'))
