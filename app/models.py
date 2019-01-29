a5506afe0270f53110225a6c6753c5bfba1f142f
jademount <linhw1978@gmail.com>
2019-01-29 22:42:17 +0800

    20190129 adding user login

--- app/models.py
+++ app/models.py
@@ -1,16 +1,29 @@
-
 from app import db
 from datetime import datetime
-class User(db.Model):
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+class User(UserMixin, db.Model):
     id=db.Column(db.Integer,primary_key=True)
     username=db.Column(db.String(64),index=True,unique=True)
     email=db.Column(db.String(120),index=True,unique=True)
     password_hash=db.Column(db.String(128))
     
     posts = db.relationship('Post', backref='author', lazy='dynamic')
-9
-def __repr__(self):
-    return '<User {}>'.format(self.username)
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
 #the __repr__ method tells Python how to print objects of this class, which is going to be useful for debugging. 
 # example:
 #>>> from app.models import User
@@ -18,11 +31,11 @@ def __repr__(self):
 #>>> u
 #<User susan>
 
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
     def __repr__(self):
         return '<Post {}>'.format(self.body)
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,11 @@
 from app import app
 from flask import render_template,flash,redirect,url_for
-
 from app.forms import LoginForm
+from flask_login import current_user, login_user
+from app.models import User
+
 @app.route("/")
 @app.route("/index")
-
 def index():
     user={'username':'Daniel'}
     posts = [{
@@ -17,11 +18,16 @@ def index():
     return render_template('index.html',title='Home',user=user, posts=posts)
 
 @app.route('/login',methods=['GET','POST'])
-
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form=LoginForm()
     #form.validate_on_submit() returns True if methods=POST, False if methods=GET 
     if form.validate_on_submit():
-        flash('User {} Login, remember me={}'.format(form.username.data,form.remember_me.data))
+        user=User.query.filter_by(form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html',title='Sign In', form=form)
