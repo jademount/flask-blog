6bc3fceec96b763fd0d3ae7602290ec8aa0375bb
jademount <linhw1978@gmail.com>
2019-02-23 15:19:29 +0800

    ch8 1st commit

---
+++ test.py
@@ -0,0 +1,11 @@
+from datetime import datetime, timedelta
+import unittest
+from app import app ,db
+from app.models import User, Post
+
+class UserModelCase(unittest.TestCase):
+    def setUp(self):
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+        db.create_all()
+        
+        
