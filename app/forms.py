bd1724c55b55b3b69c164a5fe6057ec410f2efdb
jademount <linhw1978@gmail.com>
2019-02-27 08:02:44 +0800

    20190226 ch 9 in progress

--- app/forms.py
+++ app/forms.py
@@ -33,6 +33,10 @@ class EditProfileForm(FlaskForm):
     about_me=TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit=SubmitField('Submit')
 
+class PostForm(FlaskForm):
+    post=TextAreaField('Say something', validators=[DataRequired(), Length(min=1,max=140)])
+    submit=SubmitField('Submit')
+
 
 
 
--- app/routes.py
+++ app/routes.py
@@ -2,22 +2,24 @@ from app import app,db
 from flask import render_template,flash,redirect,url_for,request
 from app.forms import LoginForm,EditProfileForm
 from flask_login import current_user, login_user,logout_user,login_required
-from app.models import User
-from app.forms import RegistrationForm
+from app.models import User,Post
+from app.forms import RegistrationForm,PostForm
 from datetime import datetime
 from werkzeug.urls import url_parse
 
-@app.route("/")
-@app.route("/index")
-#@login_required
+@app.route("/", methods=['GET','POST'] )
+@app.route("/index", methods=['GET','POST'])
+@login_required
 def index():
-    user={'username':'Daniel'}
-    posts = [{
-        'author': {'username': 'John'},
-        'body': 'Beautiful day in Portland!'},{
-        'author': {'username': 'Susan'},
-        'body': 'The Avengers movie was so cool!' }]
-    return render_template('index.html',title='Home', posts=posts)
+    form=PostForm()
+    if form.validate_on_submit():
+        post=Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+    posts=current_user.followed_posts().all()
+    return render_template('index.html',title='Home', form=form, posts=posts)
 
 @app.route('/login',methods=['GET','POST'])
 
@@ -51,7 +53,7 @@ def logout():
 
 def user(username):
     user=User.query.filter_by(username=username).first_or_404()
-    posts=[{'author':user,'body':'post No.1'},{'author':user,'body':'post No.2'}]
+    posts=current_user.followed_posts().all()
     return render_template('user.html', user=user, posts=posts)
 
 @app.route('/register', methods=['GET','POST'])
@@ -120,6 +122,12 @@ def unfollow(username):
     db.session.commit()
     flash('You are unfollowing user:{}'.format(username))
     return redirect(url_for('user',username=username))
+
+@app.route('/explore')
+@login_required
+def explore():
+    posts=Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html',title='Explore', posts=posts)
 
 
     
