f8d82ad7249c71f31fc78098e68de40ad6d278f5
jademount <linhw1978@gmail.com>
2019-03-15 22:18:02 +0800

    20190315 ch 13 datetime not translated yet

--- app/email.py
+++ app/email.py
@@ -2,6 +2,7 @@ from flask_mail import Message
 from app import mail,app
 from flask import render_template
 from threading import Thread
+from flask_babel import _
 
 def send_email(subject, sender, recipients,text_body, html_body):
     msg=Message(subject, sender=sender, recipients=recipients)
@@ -12,7 +13,7 @@ def send_email(subject, sender, recipients,text_body, html_body):
 
 def send_password_reset_email(user):
     token=user.get_reset_password_token()
-    send_email('[Microblog: reset your email]',sender=app.config['ADMINS'][0],recipients=[user.email], text_body=render_template('email/reset_password.txt', user=user, token=token), html_body=render_template('email/reset_password.html', user=user, token=token))    
+    send_email(_('[Microblog: reset your email]'),sender=app.config['ADMINS'][0],recipients=[user.email], text_body=render_template('email/reset_password.txt', user=user, token=token), html_body=render_template('email/reset_password.html', user=user, token=token))    
 
 def send_async_email(app, msg):
     with app.app_context():
